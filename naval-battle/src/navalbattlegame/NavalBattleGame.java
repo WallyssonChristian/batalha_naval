63dc02f4d8a432c6e98afd9a110a6e52f4a59f33
Wally <cerberus74@live.com>
2023-11-08 15:01:00 -0300

    :bulb: Preparatory code first method

--- naval-battle/src/navalbattlegame/NavalBattleGame.java
+++ naval-battle/src/navalbattlegame/NavalBattleGame.java
@@ -3,8 +3,24 @@ package navalbattlegame;
 public class NavalBattleGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Declarar Matriz int para armazenar os locais dos navio
+		// Declarar uma variavel int para o Score
+		// Declarar um metodo checkYourself() que use uma String para o palpite do usuário
+		// Declarar um método de configuração setShipLocation() para configurar os locais dentro da matriz de locais
+		
+		// METODO checkYourself()
+		// Capturar o palpite do usuário como um parâmetro de String
+		// converter o palpite em int 
+		// Repita para cada local da célula da matriz int:
+			// Comparar o palpite com o valor da matriz
+			// Se o palpite estiver correto = incrementar score, informar e verificar se a matriz esta vazia.
+				// Se a matriz estiver Vazia, informar que o navio afundou, fim de jogo
+			// Se o palpite estiver errado = avisar erro, incrementar score, repetir,
+		
+		// METODO setShipLocation()
+		// Capture o local da célula como parametro, utilizando uma matriz
+		// atribuir os locais na matriz de local
 	}
 
 }
